af510da667e99aaa3da747220bb092afdee012f3
Nick Winkelbauer <nwinkelb@uw.edu>
2017-07-06 10:20:24 -0700

    add media file

---
+++ setup/class.custom-page-media-json.php
@@ -0,0 +1,38 @@
+<?php
+
+add_action( 'rest_api_init', function() {
+    register_rest_field( 'page', 'featured_image_url', array(
+        'get_callback' => function( $object, $field_name, $request ) {
+            $thumbnail_id = get_post_thumbnail_id( $object->ID );
+			return wp_get_attachment_image_src($thumbnail_id, 'full');
+        }
+    ) );
+} );
+
+add_action( 'rest_api_init', function() {
+    register_rest_field( 'page', 'page_customs', array(
+        'get_callback' => function( $object, $field_name, $request ) {
+            $custom = get_post_custom( $object->ID );
+			return $custom;
+        }
+    ) );
+} );
+
+add_action( 'rest_api_init', function() {
+    register_rest_field( 'page', 'mobile_image_url', array(
+        'get_callback' => function( $object, $field_name, $request ) {
+            $custom = get_post_custom( $object->ID );
+			return wp_get_attachment_image_src($custom['mobile-id'][0], 'full');
+        }
+    ) );
+} );
+
+add_action( 'rest_api_init', function() {
+    register_rest_field( 'page', 'logo_image_url', array(
+        'get_callback' => function( $object, $field_name, $request ) {
+            $custom = get_post_custom( $object->ID );
+			return wp_get_attachment_image_src($custom['logo-id'][0], 'full');
+        }
+    ) );
+} );
+?>
