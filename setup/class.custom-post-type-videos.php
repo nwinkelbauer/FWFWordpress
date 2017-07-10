e2dcb1a8c310f2e71e8d66f1c4aabb16d18ec9db
Nick Winkelbauer <nwinkelb@uw.edu>
2017-07-10 13:12:13 -0700

    updated video post type with order

--- setup/class.custom-post-type-videos.php
+++ setup/class.custom-post-type-videos.php
@@ -55,8 +55,7 @@ if ( ! post_type_exists( 'videos' ) ):
 		add_meta_box( 'url', 'Vimeo ID Code', 'url_callback', 'videos', 'side', 'default' );
 		add_meta_box( 'date', 'Year Created', 'date_callback', 'videos', 'side', 'default' );
 		add_meta_box( 'client', 'Client', 'client_callback', 'videos', 'side', 'default' );
-
-		
+		add_meta_box( 'order', 'Order', 'order_callback', 'videos', 'side', 'default' );
 
 		remove_meta_box( 'categorydiv', 'videos', 'side' );
 		add_meta_box( 'categorydiv', 'Category', 'post_categories_meta_box', 'videos', 'normal', 'low', array( 'taxonomy' => 'category' ) );
@@ -85,12 +84,21 @@ if ( ! post_type_exists( 'videos' ) ):
 		?><input name="client" value="<?php echo $client ?>" /><?php
 	}
 
+	function order_callback() {
+		global $post;
+		$custom = get_post_custom($post->ID);
+		$order = $custom['order'][0];
+		?><input name="order" value="<?php echo $order ?>" /> </br>
+		<small>Order starts at 1, emtpy values result in page not showing. Ties are sorted by date. For those you want unordered at the bottom, just set order to 100.</small> <?php
+	}
+
 	function save_video_details() {
 		global $post;
 		if (get_post_type($post) == 'videos') {
 			update_post_meta($post->ID, 'url', $_POST['url']);
 			update_post_meta($post->ID, 'date', $_POST['date']);
 			update_post_meta($post->ID, 'client', $_POST['client']);
+			update_post_meta($post->ID, 'order', $_POST['order']);
 		}
 	}
 
