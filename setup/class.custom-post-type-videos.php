72a41b726ba716d30941cff3bf705359c2c8f3c0
Nick Winkelbauer <nwinkelb@uw.edu>
2017-09-05 20:59:49 -0700

    fixes order for categories

--- setup/class.custom-post-type-videos.php
+++ setup/class.custom-post-type-videos.php
@@ -28,13 +28,14 @@ if ( ! post_type_exists( 'videos' ) ):
 			'show_ui' => true,
 			'show_in_menu' => true,
 			'query_var' => true,
+			//'hierarchical' => true,
 			'taxonomies' => array('category'), //'post_tag' for tags
 			'rewrite' => array('slug' => 'portfolio'),
 			'has_archive' => true,
 			'show_in_rest' => true,
     		'rest_base' => 'videos',
     		'rest_controller_class' => 'WP_REST_Posts_Controller',
-			'supports' => array( 'title' , 'editor' , 'thumbnail' )
+			'supports' => array( 'title' , 'editor' , 'thumbnail', 'page-attributes' )
 		);
 
 		register_post_type('videos', $args);
@@ -53,10 +54,10 @@ if ( ! post_type_exists( 'videos' ) ):
 	add_action('admin_init', 'video_admin_init');
 
 	function video_admin_init(){
+		add_meta_box( 'order', 'Order', 'order_callback', 'videos', 'side', 'default' );
 		add_meta_box( 'url', 'Vimeo ID Code', 'url_callback', 'videos', 'side', 'default' );
 		add_meta_box( 'date', 'Year Created', 'date_callback', 'videos', 'side', 'default' );
 		add_meta_box( 'client', 'Client', 'client_callback', 'videos', 'side', 'default' );
-		add_meta_box( 'order', 'Order', 'order_callback', 'videos', 'side', 'default' );
 
 		remove_meta_box( 'categorydiv', 'videos', 'side' );
 		add_meta_box( 'categorydiv', 'Category', 'post_categories_meta_box', 'videos', 'normal', 'low', array( 'taxonomy' => 'category' ) );
@@ -87,10 +88,10 @@ if ( ! post_type_exists( 'videos' ) ):
 
 	function order_callback() {
 		global $post;
-		$custom = get_post_custom($post->ID);
-		$order = $custom['order'][0];
-		?><input name="order" value="<?php echo $order ?>" /> </br>
-		<small>Order starts at 1, emtpy values result in page not showing. Ties are sorted by date. For those you want unordered at the bottom, just set order to 100.</small> <?php
+		// $custom = get_post_custom($post->ID);
+		// $order = $custom['order'][0];
+		?><!-- <input name="order" value="<?php echo $order ?>" /> </br> -->
+		<small>Order starts at 1. Ties are sorted by date. For those you want unordered at the bottom, just set order to 100. </br></br>If you want to remove video from showing up, set status to draft or visibility to private.</small> <?php
 	}
 
 	function save_video_details() {
@@ -99,7 +100,7 @@ if ( ! post_type_exists( 'videos' ) ):
 			update_post_meta($post->ID, 'url', $_POST['url']);
 			update_post_meta($post->ID, 'date', $_POST['date']);
 			update_post_meta($post->ID, 'client', $_POST['client']);
-			update_post_meta($post->ID, 'order', $_POST['order']);
+			//update_post_meta($post->ID, 'order', $_POST['order']);
 		}
 	}
 
